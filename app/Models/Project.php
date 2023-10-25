825c6c04301d1f78808206d7772bc79ee0e5661b
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-25 17:03:40 +0200

    add function type and getTypeBadges

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+
+    }
+
+    public function getTypeBadge()
+    {
+        return $this->type ? "<span class='badge' style='background-color: {$this->type->color}'>{$this->type->label}</span>" : "Undefined";
+
+
+    }
 }
